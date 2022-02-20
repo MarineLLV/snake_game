19b9197fa83b3837d6afdb07a524abbf70cee340
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-20 18:02:35 +0100

    Add play again feature

--- main.py
+++ main.py
@@ -4,44 +4,67 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("#E4E6AC")
-screen.title("Welcome to Snake Game!")
-screen.tracer(0)
+def game():
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("#E4E6AC")
+    screen.title("Welcome to Snake Game!")
+    screen.tracer(0)
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
+
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
-    # Detect collision with wall
-    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
-        game_is_on = False
-        scoreboard.game_over()
-
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
+        # Detect collision with wall
+        if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
             scoreboard.game_over()
+            game_is_on = False
+            again = screen.textinput("Want to play again?", "Y / N")
 
-screen.exitonclick()
+            if again in ("y", "Y"):
+                game_is_on = True
+                screen.clear()
+                screen.bgcolor("#E4E6AC")
+                game()
+            else:
+                exit()
+
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                scoreboard.game_over()
+                game_is_on = False
+                again = screen.textinput("Want to play again?", "Y / N")
+
+                if again in ("y", "Y"):
+                    game_is_on = True
+                    screen.clear()
+                    screen.bgcolor("Black")
+                    game()
+                else:
+                    exit()
+
+    screen.exitonclick()
+
+
+game()
