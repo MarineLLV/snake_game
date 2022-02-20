7e2216bbb2cfd8da4c8767ed3ba520c66e72dc8c
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-20 01:54:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Welcome to Snake Game!")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
