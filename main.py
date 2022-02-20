96543541037fe7f98b76fd8c9b2e2314e6793383
MarineLLV <marine.lucottelevisage@gmail.com>
2022-02-20 15:50:48 +0100

    Collision with wall

--- main.py
+++ main.py
@@ -32,5 +32,10 @@ while game_is_on:
         food.refresh()
         scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,10 @@ class Scoreboard(Turtle):
     def update_scoreborad(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
     def increase_score(self):
         self.score += 1
         self.clear()
